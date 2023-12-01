36a562d817a2c3b5619889286f561c5ed7c9a6ba
sava <sava.stevanovic@pmf.edu.rs>
2023-12-01 23:05:59 +0100

    Automatic header detection

--- project/extractors/dataframe/spliter.py
+++ project/extractors/dataframe/spliter.py
@@ -20,6 +20,28 @@ class RowSpaceSpliter(TableExtactor):
             else:
                 empty_row_count = 0
         split_indices.append(len(table))
-        split_dfs = [table.iloc[split_indices[i-1]:split_indices[i], :] for i in range(1, len(split_indices))]
+        split_dfs = [
+            table.iloc[split_indices[i - 1] : split_indices[i], :]
+            for i in range(1, len(split_indices))
+        ]
 
         return split_dfs
+
+
+class PaddingRemover(TableExtactor):
+    def __call__(self, table: pd.DataFrame) -> typing.List[pd.DataFrame]:
+        table = table.dropna(axis=0, how="all")
+        table = table.dropna(axis=1, how="all")
+
+        return [table]
+
+
+class AutoHeader(TableExtactor):
+    def __call__(self, table: pd.DataFrame) -> typing.List[pd.DataFrame]:
+        row_strs = table.apply(
+            lambda row: sum(isinstance(cell, str) for cell in row), axis=1
+        )
+        header_col = row_strs.to_numpy().argmax()
+        table.columns = table.iloc[header_col]
+        table = table.iloc[header_col + 1 :]
+        return [table]
--- project/extractors/extractor.py
+++ project/extractors/extractor.py
@@ -8,9 +8,22 @@ class TableExtactor:
     def __call__(self, table: pd.DataFrame) -> typing.List[pd.DataFrame]:
         pass
 
+
 class ListExtactor:
     def __init__(self, extractor: TableExtactor) -> None:
         self._extractor = extractor
 
     def __call__(self, tables: typing.List[pd.DataFrame]) -> typing.List[pd.DataFrame]:
         return [self._extractor(table) for table in tables]
+
+
+class Processor:
+    def __init__(self, extractors: typing.List[TableExtactor]) -> None:
+        self._extractors = extractors
+
+    def __call__(self, tables: typing.List[pd.DataFrame]) -> typing.List[pd.DataFrame]:
+        for extractor in self._extractors:
+            tables = ListExtactor(extractor)(tables)
+            tables = sum(tables, [])
+
+        return tables
