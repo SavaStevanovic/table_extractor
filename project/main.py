d5c659953c01ecb85e57ea9c8ae7f82a54e4d33e
sava <sava.stevanovic@pmf.edu.rs>
2023-12-03 13:24:58 +0100

    Config for new excell file

--- project/main.py
+++ project/main.py
@@ -30,7 +30,37 @@ out = Processor(
     ]
 )(test)
 
+tables = ["data/Table#2.xlsx"]
+test = sum(
+    [list(pd.read_excel(t, sheet_name=None, header=None).values()) for t in tables], []
+)
+
+out += Processor(
+    [
+        spliter.PaddingRemover(),
+        spliter.AutoHeader(DownRight("\d{4}[-/]\d{2,4}", [])),
+        spliter.ColumnClean(),
+        spliter.ColumnRename(
+            {"WAREHOUSE": "NAME", "LOCATION NAME IDENTIFIER": "NAME", "ADDRESS": "CITY"}
+        ),
+        spliter.IndexSet(["NAME", "CITY"]),
+        spliter.HeaderDataExtractor(
+            "SEASON",
+            {
+                "2020 21": 2020,
+                "2021 22": 2021,
+                "2022 23": 2022,
+                "2023 24": 2023,
+                "2020 2021": 2020,
+                "2021 2022": 2021,
+                "2022 2023": 2022,
+                "2023 2024": 2023,
+            },
+        ),
+        spliter.DropNanIndex(),
+        spliter.ColumnClean(),
+    ]
+)(test)
+
 for i, o in enumerate(out):
     o.to_csv(f"{i}.csv")
-total_out = Merger(["NAME", "CITY"], ["SEASON"])(out)
-total_out.to_csv("total.csv")
